8217524cff8daf5da16eee262fc062b19ea96f5b
Jonas Bark <jonas.bark@gmx.de>
2025-11-29 13:35:57 +0000

    move service running message

--- lib/pages/trainer.dart
+++ lib/pages/trainer.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:swift_control/main.dart';
+import 'package:swift_control/utils/actions/android.dart';
 import 'package:swift_control/utils/core.dart';
 import 'package:swift_control/utils/keymap/apps/my_whoosh.dart';
 import 'package:swift_control/utils/requirements/android.dart';
@@ -11,6 +12,8 @@ import 'package:swift_control/utils/requirements/remote.dart';
 import 'package:swift_control/widgets/apps/mywhoosh_link_tile.dart';
 import 'package:swift_control/widgets/apps/zwift_tile.dart';
 import 'package:swift_control/widgets/ui/connection_method.dart';
+import 'package:swift_control/widgets/ui/warning.dart';
+import 'package:url_launcher/url_launcher_string.dart' show launchUrlString;
 
 class TrainerPage extends StatefulWidget {
   final VoidCallback onUpdate;
@@ -21,6 +24,8 @@ class TrainerPage extends StatefulWidget {
 }
 
 class _TrainerPageState extends State<TrainerPage> {
+  bool? _isRunningAndroidService;
+
   @override
   void initState() {
     super.initState();
@@ -36,6 +41,13 @@ class _TrainerPageState extends State<TrainerPage> {
       if (core.settings.getZwiftEmulatorEnabled() && core.actionHandler.supportedApp?.supportsZwiftEmulation == true) {
         core.zwiftEmulator.startAdvertising(() {
           if (mounted) setState(() {});
+        });
+      }
+      if (Platform.isAndroid && core.actionHandler is AndroidActions) {
+        (core.actionHandler as AndroidActions).accessibilityHandler.isRunning().then((isRunning) {
+          setState(() {
+            _isRunningAndroidService = isRunning;
+          });
         });
       }
     }
@@ -56,7 +68,46 @@ class _TrainerPageState extends State<TrainerPage> {
               description:
                   'Enable keyboard and mouse control for better interaction with ${core.settings.getTrainerApp()?.name}.',
               requirements: [Platform.isAndroid ? AccessibilityRequirement() : KeyboardRequirement()],
-              onChange: (value) {},
+              isStarted: _isRunningAndroidService == true,
+              onChange: (value) {
+                (core.actionHandler as AndroidActions).accessibilityHandler.isRunning().then((isRunning) {
+                  setState(() {
+                    _isRunningAndroidService = isRunning;
+                  });
+                });
+              },
+              additionalChild: _isRunningAndroidService == false
+                  ? Warning(
+                      children: [
+                        Text('Accessibility Service is not running.\nFollow instructions at').xSmall,
+                        Row(
+                          spacing: 8,
+                          children: [
+                            Expanded(
+                              child: LinkButton(
+                                child: Text('dontkillmyapp.com'),
+                                onPressed: () {
+                                  launchUrlString('https://dontkillmyapp.com/');
+                                },
+                              ),
+                            ),
+                            IconButton.secondary(
+                              onPressed: () {
+                                (core.actionHandler as AndroidActions).accessibilityHandler.isRunning().then((
+                                  isRunning,
+                                ) {
+                                  setState(() {
+                                    _isRunningAndroidService = isRunning;
+                                  });
+                                });
+                              },
+                              icon: Icon(Icons.refresh),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  : null,
             ),
           ),
         if (core.settings.getTrainerApp() is MyWhoosh && core.whooshLink.isCompatible(core.settings.getLastTarget()!))
