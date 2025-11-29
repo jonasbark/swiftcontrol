bdd7c75ebd5f20c462a85affdae66d0ae5d0817b
Jonas Bark <jonas.bark@gmx.de>
2025-11-29 21:54:24 +0000

    attempt to support Wahoo Kickr Bike Pro #195

--- lib/bluetooth/devices/wahoo/wahoo_kickr_bike_pro.dart
+++ lib/bluetooth/devices/wahoo/wahoo_kickr_bike_pro.dart
@@ -1,5 +1,10 @@
-import 'package:swift_control/bluetooth/devices/zwift/zwift_play.dart';
+import 'package:swift_control/bluetooth/devices/zwift/zwift_ride.dart';
 
-class WahooKickrBikePro extends ZwiftPlay {
+import '../zwift/constants.dart';
+
+class WahooKickrBikePro extends ZwiftRide {
   WahooKickrBikePro(super.scanResult) : super();
+
+  @override
+  String get customServiceId => ZwiftConstants.ZWIFT_CUSTOM_SERVICE_UUID;
 }
