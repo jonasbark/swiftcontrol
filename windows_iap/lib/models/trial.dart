38df962e43b641bc540959926dbf354d94b44d54
jonas.bark@gmx.de <jonas.bark@gmx.de>
2025-12-21 10:25:52 +0100

    windows trial changes

--- windows_iap/lib/models/trial.dart
+++ windows_iap/lib/models/trial.dart
@@ -1,6 +1,8 @@
 class Trial {
   final bool isTrial;
   final int remainingDays;
+  final bool isActive;
+  final bool isTrialOwnedByThisUser;
 
-  Trial({required this.isTrial, required this.remainingDays});
+  Trial({required this.isTrial, required this.remainingDays, required this.isActive, required this.isTrialOwnedByThisUser});
 }
