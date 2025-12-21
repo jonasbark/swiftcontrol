cb25318bae56cd96adb56f1382bb37cd2b07645e
Jonas Bark <jonas.bark@gmx.de>
2025-12-21 10:29:53 +0100

    windows adjustments

--- windows_iap/lib/models/trial.dart
+++ windows_iap/lib/models/trial.dart
@@ -4,5 +4,15 @@ class Trial {
   final bool isActive;
   final bool isTrialOwnedByThisUser;
 
-  Trial({required this.isTrial, required this.remainingDays, required this.isActive, required this.isTrialOwnedByThisUser});
+  Trial({
+    required this.isTrial,
+    required this.remainingDays,
+    required this.isActive,
+    required this.isTrialOwnedByThisUser,
+  });
+
+  @override
+  String toString() {
+    return 'Trial{isTrial: $isTrial, remainingDays: $remainingDays, isActive: $isActive, isTrialOwnedByThisUser: $isTrialOwnedByThisUser}';
+  }
 }
