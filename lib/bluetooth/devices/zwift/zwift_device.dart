9612b213aa864dc5dec74368bd7720748a9b9e94
Jonas Bark <jonas.bark@gmx.de>
2026-01-13 11:49:27 +0100

    adjust instructions

--- lib/bluetooth/devices/zwift/zwift_device.dart
+++ lib/bluetooth/devices/zwift/zwift_device.dart
@@ -89,7 +89,7 @@ abstract class ZwiftDevice extends BluetoothDevice {
 
   @override
   Future<void> processCharacteristic(String characteristic, Uint8List bytes) async {
-    if (kDebugMode) {
+    if (kDebugMode && false) {
       actionStreamInternal.add(
         LogNotification(
           "Received data on $characteristic: ${bytes.map((e) => e.toRadixString(16).padLeft(2, '0')).join(' ')}",
