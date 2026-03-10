45dc17659fd5db4c0d0e4071ea626e034b405f88
Jonas Bark <jonas.bark@gmx.de>
2026-03-10 08:15:51 +0100

    add support for Kickr Bike V1 + V2
    # Conflicts:
    #	CHANGELOG.md

--- lib/bluetooth/devices/wahoo/wahoo_kickr_bike_pro.dart
+++ lib/bluetooth/devices/wahoo/wahoo_kickr_bike_pro.dart
@@ -1,10 +1,5 @@
 import 'package:bike_control/bluetooth/devices/zwift/zwift_ride.dart';
 
-import '../zwift/constants.dart';
-
 class WahooKickrBikePro extends ZwiftRide {
   WahooKickrBikePro(super.scanResult) : super();
-
-  @override
-  String get customServiceId => ZwiftConstants.ZWIFT_CUSTOM_SERVICE_UUID;
 }
