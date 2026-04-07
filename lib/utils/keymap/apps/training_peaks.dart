ba7525efcd75ce8411e2e35a2d8e8665b111c3b9
Jonas Bark <jonas.bark@gmx.de>
2026-04-07 09:05:25 +0200

    degrade buggy TrainingPeaks support

--- lib/utils/keymap/apps/training_peaks.dart
+++ lib/utils/keymap/apps/training_peaks.dart
@@ -14,7 +14,7 @@ import '../keymap.dart';
 class TrainingPeaks extends SupportedApp {
   @override
   List<(AppConnectionMethod, ConnectionSupport)> get connections => [
-    (AppConnectionMethod.obpBle, ConnectionSupport.supported),
+    (AppConnectionMethod.obpBle, ConnectionSupport.experimental),
     (AppConnectionMethod.obpDirCon, ConnectionSupport.experimental),
   ];
 
