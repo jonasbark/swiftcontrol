76bf2ee8d6d570d63360d1db0cb20479844de367
Jonas Bark <jonas.bark@gmx.de>
2026-04-25 20:18:31 +0200

    feat: declare TrainerConnection.virtualShiftingTransport

--- lib/bluetooth/devices/trainer_connection.dart
+++ lib/bluetooth/devices/trainer_connection.dart
@@ -1,4 +1,5 @@
 import 'package:bike_control/utils/actions/base_actions.dart';
+import 'package:bike_control/utils/keymap/apps/supported_app.dart';
 import 'package:bike_control/utils/keymap/buttons.dart';
 import 'package:bike_control/utils/keymap/keymap.dart';
 import 'package:bike_control/widgets/ui/connection_method.dart';
@@ -15,6 +16,18 @@ abstract class TrainerConnection {
   final ValueNotifier<bool> isConnected = ValueNotifier(false);
 
   TrainerConnection({required this.title, required this.type, required this.supportedActions});
+
+  /// Which Bridge (Virtual Shifting) transport this connection actually rides on.
+  /// Used by [CoreLogic.preferredBridgeTransport] to decide whether a Virtual
+  /// Shifting session over this app should advertise via WiFi (FTMS/mDNS) or
+  /// Bluetooth (BLE peripheral). `null` for connection methods that don't carry
+  /// trainer telemetry (e.g. [ConnectionMethodType.local]).
+  TrainerConnectionType? get virtualShiftingTransport => switch (type) {
+        ConnectionMethodType.bluetooth => TrainerConnectionType.bluetooth,
+        ConnectionMethodType.network => TrainerConnectionType.wifi,
+        ConnectionMethodType.openBikeControl => null,
+        ConnectionMethodType.local => null,
+      };
 
   Future<ActionResult> sendAction(KeyPair keyPair, {required bool isKeyDown, required bool isKeyUp});
 
