e96cdb157f9fdf8343c4aef7850eb8b90fc92087
Jonas Bark <jonas.bark@gmx.de>
2026-05-08 23:00:45 +0200

    feat(overlay): platform router and global overlay-mode notifier

---
+++ test/services/overlay/trainer_overlay_service_test.dart
@@ -0,0 +1,21 @@
+import 'package:bike_control/services/overlay/trainer_overlay_controller.dart';
+import 'package:bike_control/services/overlay/trainer_overlay_service.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('returns NoOpOverlayController on web/test platforms', () {
+    if (kIsWeb) {
+      final c = TrainerOverlayService.forCurrentPlatform();
+      expect(c, isA<NoOpOverlayController>());
+    } else {
+      final c1 = TrainerOverlayService.forCurrentPlatform();
+      final c2 = TrainerOverlayService.forCurrentPlatform();
+      expect(identical(c1, c2), isTrue);
+    }
+  });
+
+  test('trainerOverlayMode notifier exists and starts false', () {
+    expect(trainerOverlayMode.value, isFalse);
+  });
+}
